19bfe44af5f7ee7bd5036857da1bf5c38a8a8b77
=Deviator <deviator206@gmail.com>
2020-01-05 16:46:00 +0530

    changes for invoice

---
+++ gpc/precache-manifest.4bce02b25089d49632d5775ffcb5d614.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691e4b7ff3e876b47c86401f08fd0311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f751c7e7e09e55aa895c",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1ff9dbdbe3da57af18ee",
+    "url": "/static/css/main.74ae4b80.chunk.css"
+  },
+  {
+    "revision": "f751c7e7e09e55aa895c",
+    "url": "/static/js/2.906d5195.chunk.js"
+  },
+  {
+    "revision": "d8bc4188dd8455791c16ac0abd91133e",
+    "url": "/static/js/2.906d5195.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ff9dbdbe3da57af18ee",
+    "url": "/static/js/main.608bdde4.chunk.js"
+  },
+  {
+    "revision": "8374bf66153d849f0da8",
+    "url": "/static/js/runtime-main.d1a34089.js"
+  }
+]);
