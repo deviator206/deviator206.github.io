6671f86aadbf4cac634662b3754a27621ecc075e
=Deviator <deviator206@gmail.com>
2020-01-19 16:54:52 +0530

    changes for nested deploy

---
+++ gpc/precache-manifest.0191c55f556d4b4a4f1375dbc04ee2f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff67aad6b0eb56efe5f185b2bfe59a2",
+    "url": "/gpc/index.html"
+  },
+  {
+    "revision": "765c617f72a1d68f6ce6",
+    "url": "/gpc/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4a128b6a35e92b5127ad",
+    "url": "/gpc/static/css/main.6a6ef9f0.chunk.css"
+  },
+  {
+    "revision": "765c617f72a1d68f6ce6",
+    "url": "/gpc/static/js/2.8590826c.chunk.js"
+  },
+  {
+    "revision": "d8bc4188dd8455791c16ac0abd91133e",
+    "url": "/gpc/static/js/2.8590826c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a128b6a35e92b5127ad",
+    "url": "/gpc/static/js/main.0f3c665a.chunk.js"
+  },
+  {
+    "revision": "1f761061d17bd8433014",
+    "url": "/gpc/static/js/runtime-main.bf9c473d.js"
+  },
+  {
+    "revision": "dbd9b8b1fedac8be967b362bb3ab39e5",
+    "url": "/gpc/static/media/Capture.dbd9b8b1.PNG"
+  }
+]);
--- gpc/service-worker.js
+++ gpc/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b16ec25f18018dc59ef5395db1f9629.js"
+  "/gpc/precache-manifest.0191c55f556d4b4a4f1375dbc04ee2f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gpc/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
